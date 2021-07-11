396985be79a5b5836ebf04c57fe250126425035b
DarthBenro008 <hkpdev008@gmail.com>
2021-07-11 16:14:31 +0530

    feat: basic rchore feature

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod cli;
+mod tasks;
+
 use structopt::StructOpt;
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 fn main() {
-    cli::CommandLineArgs::from_args();
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add {text} => tasks::add_task(journal_file, Task::new(text)),
+        Done {position} => tasks::complete_task(journal_file, position),
+        List => tasks::list_tasks(journal_file)
+    }.expect("Failed to perform the required action")
 }
